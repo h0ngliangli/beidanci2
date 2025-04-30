88b262b9cf4c0ddbcb29bf17701c84d2b47049ad
Hongliang Li <woodyli1982@gmail.com>
2025-04-30 11:25:09 -0700

    创建ColorModeButton组件

---
+++ components/ColorModeButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <ClientOnly>
+        <UButton
+            :icon="isDark ? 'i-lucide-moon': 'i-lucide-sun'"
+            color="neutral"
+            size="xl"
+            variant="ghost"
+            @click="isDark = !isDark"
+        />
+    </ClientOnly>
+</template>
+
+<script setup>
+const colorMode = useColorMode()
+const isDark = computed({
+    get() {
+        return colorMode.value === 'dark'
+    },
+    set(_isDark) {
+        colorMode.value = _isDark ? 'dark' : 'light'
+    }
+})
+</script>
